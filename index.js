113c97dcdc44e21af9b148d73670405586e5524f
Wálisson Andrey <85573798+PennyWASS@users.noreply.github.com>
2022-12-16 10:08:37 -0300

    Atualizando o index.js
    Coloquei funcionalidade para isso e também modifiquei todas as páginas para funcionarem com o node JS.

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const express = require('express') // importando a biblioteca 'express'
+const express = require('express'); // importando a biblioteca 'express'
 
 const server = express(); // importando a biblioteca 'server'
 
-server.get('/home', ()=>{
-    console.log('Você está tentando acessar a página home...')
+const path = require('path');
+// server.get('/home', ()=>{
+//     console.log('Você está tentando acessar a página home...')
+// })
+
+server.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname+'/paginas/index.html'))
 })
 
-server.get('/', ()=>{
-    console.log('Você está tentando acessar a página inicial.')
+server.get('/desenvolvedores', (req, res)=>{
+    res.sendFile(path.join(__dirname+'/paginas/desenvolvedores.html'))
 })
 
-server.get('/login', ()=>{
-    console.log('Você está tentando acessar a página de login.')
+server.get('/inscricao', (req, res)=>{
+    res.sendFile(path.join(__dirname+'/paginas/inscricao.html'))
 })
 
-server.get('/perfil', ()=>{
-    console.log('Você está tentando acessar a página de perfil.')
+server.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname+'/paginas/login.html'))
 })
 
-server.get('/', ()=>{
-    console.log('Você está tentando acessar a página inicial.')
+server.get('/perfil', (req, res)=>{
+    res.sendFile(path.join(__dirname+'/paginas/perfil.html'))
 })
 
 server.listen(3000)
