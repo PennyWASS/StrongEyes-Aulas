2f636a96e8909367e144b747748169fc6ca54249
Wálisson Andrey <85573798+PennyWASS@users.noreply.github.com>
2023-01-26 10:58:43 -0300

    Correção na página de inscrição

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const server = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const database = require('./db');
+const database2 = require('./db2');
 const Perfil = require('./modelos/perfil');
+const Perfil2 = require('./modelos/perfil2');
 const alert = require('alert');
 
 server.use(express.static(path.join(__dirname, 'public')));
@@ -24,6 +26,16 @@ server.use(bodyParser.urlencoded({
     console.log(error);
  }
 })();
+(async() => {
+ 
+    try{
+       const resultado = await database2.sync();
+       console.log(resultado);
+   
+    }catch(error){
+       console.log(error);
+    }
+   })();
 var createPerfil = {};
 server.post('/salvarPerfil', async(req, res)=>{
     const createPerfil = await Perfil.create({
@@ -40,7 +52,39 @@ server.post('/salvarPerfil', async(req, res)=>{
         senha: req.body.senha
     });
     console.log(createPerfil);
-    res.sendFile(path.join(__dirname+'/public/paginas/index.html'));
+    res.sendFile(path.join(__dirname+'/public/paginas/login.html'));
+})
+
+var createPerfil2 = {};
+server.post('/salvarPerfil2', async(req, res)=>{
+    const createPerfil2 = await Perfil2.create({
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        // telefone: req.body.telefone,
+        // estado: req.body.estado,
+        // email: req.body.email,
+        idade: req.body.idade,
+        pais: req.body.pais,
+        login: req.body.email,
+        senha: req.body.senha
+    });
+    console.log(createPerfil2);
+    res.sendFile(path.join(__dirname+'/public/paginas/login.html'));
+})
+
+server.post('/logar2', async(req, res)=>{
+    const selecUsuario = await Perfil2.findOne({
+        where:{
+            login: req.body.email,
+            senha: req.body.senha
+        }
+    });
+    if(selecUsuario !== null){
+        res.sendFile(path.join(__dirname+'/public/paginas/index.html'))
+    } else{
+        alert('Login ou senha incorretos'),
+        res.sendFile(path.join(__dirname+'/public/paginas/login.html'))
+    }
 })
 
 server.post('/logar', async(req, res)=>{
---
+++ modelos/perfil2.js
@@ -0,0 +1,61 @@
+const Sequelize = require('sequelize');
+const database = require("../db2");
+
+const Perfil2 = database.define('perfil', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true    
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    // sobrenome:{
+    //     type: Sequelize.STRING,
+    //     allowNull: false
+    // }, 
+    // telefone:{
+    //     type: Sequelize.STRING,
+    //     allowNull: false
+    // }, 
+    // bairro:{
+    //     type: Sequelize.STRING,
+    //     allowNull: false
+    // }, 
+    // rua:{
+    //     type: Sequelize.STRING,
+    //     allowNull: false
+    // }, 
+    // cep:{
+    //     type: Sequelize.STRING,
+    //     allowNull: false
+    // }, 
+    // estado:{
+    //     type: Sequelize.STRING,
+    //     allowNull: false
+    // }, 
+    // email:{
+    //     type: Sequelize.STRING,
+    //     allowNull: false
+    // }, 
+    pais:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    idade:{
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    login:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, 
+    senha:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+module.exports = Perfil2;
